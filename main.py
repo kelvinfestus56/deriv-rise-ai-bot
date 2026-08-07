2210b73bed71321399696b9198c0ba9fe0c5c379
kelvinfestus56 <kelvinfestus56@gmail.com>
2026-08-07 20:46:48 +0300

    Update main.py

--- main.py
+++ main.py
@@ -1,11 +1,19 @@
+from engine1.technical import TechnicalEngine
+
 import asyncio
 import json
 import websockets
 
+
 DERIV_WS = "wss://ws.derivws.com/websockets/v3?app_id=1089"
 
+
 async def connect_deriv():
+
+    analyzer = TechnicalEngine()
+
     async with websockets.connect(DERIV_WS) as ws:
+
         print("Connected to Deriv")
 
         request = {
@@ -15,13 +23,26 @@ async def connect_deriv():
         await ws.send(json.dumps(request))
 
         while True:
+
             data = await ws.recv()
             tick = json.loads(data)
 
             if "tick" in tick:
-                price = tick["tick"]["quote"]
-                epoch = tick["tick"]["epoch"]
 
-                print("Time:", epoch, "Price:", price)
+                price = tick["tick"]["quote"]
+
+                analyzer.add_tick(price)
+
+                analysis = analyzer.analyze()
+
+                print(
+                    "Price:",
+                    price,
+                    "| Direction:",
+                    analysis["direction"],
+                    "| Score:",
+                    analysis["score"]
+                )
+
 
 asyncio.run(connect_deriv())
