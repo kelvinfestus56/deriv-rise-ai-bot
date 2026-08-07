422152f32613b04f9af4e1ad11ab935a490e931f
kelvinfestus56 <kelvinfestus56@gmail.com>
2026-08-07 20:55:40 +0300

    Update decision_engine.py

--- engine3/decision_engine.py
+++ engine3/decision_engine.py
@@ -1,46 +1,28 @@
-class AIEngine:
+class DecisionEngine:
 
     def __init__(self):
-        self.history = []
+        self.minimum_confidence = 75
 
-    def analyze(self, technical_result):
 
-        direction = technical_result["direction"]
-        tech_score = technical_result["score"]
+    def decide(self, technical, ai):
 
-        if direction == "UP":
-            ai_score = tech_score + 10
+        technical_score = technical["score"]
+        ai_score = ai["ai_score"]
 
-        elif direction == "DOWN":
-            ai_score = tech_score - 10
+
+        confidence = (
+            technical_score + ai_score
+        ) / 2
+
+
+        if confidence >= self.minimum_confidence:
+            decision = "BUY RISE"
 
         else:
-            ai_score = 50
-
-
-        if ai_score > 100:
-            ai_score = 100
-
-        if ai_score < 0:
-            ai_score = 0
+            decision = "WAIT"
 
 
         return {
-            "ai_score": ai_score,
-            "analysis": self.get_message(ai_score)
+            "decision": decision,
+            "confidence": round(confidence, 2)
         }
-
-
-    def get_message(self, score):
-
-        if score >= 80:
-            return "Strong UP setup"
-
-        elif score >= 60:
-            return "Possible UP setup"
-
-        elif score <= 30:
-            return "Weak setup"
-
-        else:
-            return "Wait for better condition"
